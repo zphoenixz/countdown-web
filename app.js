54346120b405c522ed696f7cfdc1cbb8ab63a8b3
zphoenixz <ing.ramiro.valdez.a@gmail.com>
2019-08-24 19:56:37 +0200

    cleanning

--- app.js
+++ app.js
@@ -12,21 +12,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-// const item1 = new Item({
-//     name: "Welcome to your todolist!"
-// });
-// const item2 = new Item({
-//     name: "Hit the + button to add a new item."
-// });
-// const item3 = new Item({
-//     name: "<-- Hit this to delete an item."
-// });
-// const defaultItems = [item1, item2, item3];
-// const listSchema = {
-//     name: String,
-//     items: [itemsSchema]
-// };
-
 
 app.get("/", function (req, res) {
     res.render("index");
